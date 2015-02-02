00cba18583d7dd1ae66c05c7f9ae109050a977f5
yichen <yichenzhu1337@gmail.com>
2015-02-01 20:32:41 -0500

    full complete frontend rework

--- app/views/index.php
+++ app/views/index.php
@@ -2,39 +2,27 @@
 <html>
 
     <head>
-        <title> My Esports World </title>
+        <title> Wiseproject </title>
+
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="mew is awesome">
+        <meta name="description" content="wp is awesome">
         <meta name="author" content="yichen">
+
+        <!-- CSS -->
         <link rel="shortcut icon" href="app/images/favicon.ico">
         <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
         <link href="app/css/styles.css" rel="stylesheet" type="text/css"/>
         <link href="app/css/animations.css" rel="stylesheet" type="text/css"/>
     </head>
 
-    <body ng-app="MewApp">
+    <body ng-app="app">
 
-        <div class="container">
-
-            <div ng-view class="slide-animation"> my SPA </div>
-
-            <div> <?php echo Session::token(); ?> </div>
-            <div> <?php echo csrf_token(); ?> </div>
-
-        </div>
-
-        <!-- Bower Dependencies-->
-        <script src="bower_components/jquery/dist/jquery.min.js"></script>
-        <script src="bower_components/angular/angular.min.js"></script>
-        <script src="bower_components/angular-route/angular-route.min.js"></script>
-        <script src="bower_components/angular-animate/angular-animate.min.js"></script>
-
-        <!-- MEW App -->
-        <script src="app/Modules/routes.js"></script>
-        <script src="app/Modules/values.js"></script>
+        <div> <?php echo Session::token(); ?> </div>
+        <div> <?php echo csrf_token(); ?> </div>
 
     </body>
+    
 </html>
